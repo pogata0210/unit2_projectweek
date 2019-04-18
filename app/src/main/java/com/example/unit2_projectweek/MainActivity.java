c59b4df0b3647fe415bbf741c6f3473d5bc3f6d5
Pablo Ogata <pabloogata@gmail.com>
2019-04-18 16:06:33 -0400

    building and running, crashes on click

--- app/src/main/java/com/example/unit2_projectweek/MainActivity.java
+++ app/src/main/java/com/example/unit2_projectweek/MainActivity.java
@@ -1,58 +1,126 @@
 package com.example.unit2_projectweek;
+import android.Manifest;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button playButton, pauseButton, stopButton;
-    MediaPlayer mediaPlayer;
-    int pausePosition;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myListViewForSongs;
+    String[] items;
+    EditText searchBar, addBar;
+
+    Button addButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playButton = (Button) findViewById(R.id.play_button);
-        pauseButton = (Button) findViewById(R.id.pause_button);
-        stopButton = (Button) findViewById(R.id.stop_button);
+        myListViewForSongs = (ListView) findViewById(R.id.mySongListView);
 
-        playButton.setOnClickListener(this);
-        pauseButton.setOnClickListener(this);
-        stopButton.setOnClickListener(this);
+
+        addButton = (Button) findViewById(R.id.add_button);
+
+        runtimePermission();
+
+
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.play_button:
+    public void runtimePermission() {
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
 
-                if (mediaPlayer == null) {
-                    mediaPlayer = mediaPlayer.create(getApplicationContext(), R.raw.im_yours);
-                    mediaPlayer.start();
-                } else if(!mediaPlayer.isPlaying()) {
-                    mediaPlayer.seekTo(pausePosition);
-                    mediaPlayer.start();
+                        display();
 
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+
+
+    }
+
+
+    public ArrayList<File> findSong(File file) {
+
+        ArrayList<File> arrayList = new ArrayList<>();
+
+        File[] files = file.listFiles();
+
+        for (File singleFile : files) {
+            if (singleFile.isDirectory() && !singleFile.isHidden()) {
+                arrayList.addAll(findSong(singleFile));
+            } else {
+                if (singleFile.getName().endsWith(".mp3") ||
+                        singleFile.getName().endsWith(".wav")) {
+
+                    arrayList.add(singleFile);
                 }
-                break;
 
-            case R.id.pause_button:
-                if(mediaPlayer!=null) {
-                    mediaPlayer.pause();
-                    pausePosition=mediaPlayer.getCurrentPosition();
+            }
 
-                }
-                break;
-
-            case R.id.stop_button:
-                if (mediaPlayer == null) {
-                    mediaPlayer.stop();
-                    mediaPlayer=null;
-                }
-                break;
         }
+        return arrayList;
+
+
+    }
+
+    void display() {
+
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+
+        items = new String[mySongs.size()];
+
+        for (int i = 0; i < mySongs.size(); i++) {
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+        }
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        myListViewForSongs.setAdapter(myAdapter);
+
+        myListViewForSongs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+
+                String songName = myListViewForSongs.getItemAtPosition(i).toString();
+
+                startActivity(new Intent(getApplicationContext(), PlayerActivity.class)
+                        .putExtra("songs", mySongs).putExtra("songname", songName)
+                        .putExtra("pos", i));
+            }
+
+        });
     }
 }
